aa51b49fd9fee8a39c261bbfd4786304957c1efa
Daniil <ivanovvdaniil2024@gmail.com>
2025-04-08 11:08:21 +0300

    Initial commit

---
+++ script.js
@@ -0,0 +1,10 @@
+// Плавна прокрутка до секцій
+document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+    anchor.addEventListener('click', function (e) {
+        e.preventDefault();
+
+        document.querySelector(this.getAttribute('href')).scrollIntoView({
+            behavior: 'smooth'
+        });
+    });
+});
