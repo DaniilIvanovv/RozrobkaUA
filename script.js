62810cfc06a9d72d08fd390eb9e9e3a01c683ce8
Daniil <ivanovvdaniil2024@gmail.com>
2025-04-11 09:49:55 +0300

    1.0

--- script.js
+++ script.js
@@ -1,10 +1,25 @@
-// Плавна прокрутка до секцій
-document.querySelectorAll('a[href^="#"]').forEach(anchor => {
-    anchor.addEventListener('click', function (e) {
-        e.preventDefault();
+const themeToggle = document.getElementById('theme-toggle');
+const body = document.body;
 
-        document.querySelector(this.getAttribute('href')).scrollIntoView({
-            behavior: 'smooth'
-        });
-    });
+function updateThemeButtonText() {
+    themeToggle.textContent = body.classList.contains('dark-theme') ? 'Денна тема' : 'Нічна тема';
+}
+
+themeToggle.addEventListener('click', () => {
+    body.classList.toggle('dark-theme');
+
+    if (body.classList.contains('dark-theme')) {
+        localStorage.setItem('theme', 'dark');
+    } else {
+        localStorage.setItem('theme', 'light');
+    }
+
+    updateThemeButtonText();
 });
+
+const savedTheme = localStorage.getItem('theme');
+if (savedTheme === 'dark') {
+    body.classList.add('dark-theme');
+}
+
+updateThemeButtonText();
